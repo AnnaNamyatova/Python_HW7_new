131e1b1b4dbac60e376b20171c3eda07c9464e35
AnnaNamyatova <37295536+AnnaNamyatova@users.noreply.github.com>
2018-11-13 00:00:17 +0300

    Update DNA_RNA_classes_first_commit.py

--- DNA_RNA_classes_first_commit.py
+++ DNA_RNA_classes_first_commit.py
@@ -2,39 +2,39 @@ class Dna(str):  #The uploaded DNA should run from 3' to 5' end
     
     
     def __init__(self, dna_seq):  
-        self.dna_seq = dna_seq
+        self.dna_seq = dna_seq.upper()
         codes = 'AaCcGgTtRrWwSsYyKkVvHhDdBbNn'
         for i in dna_seq:            
             if i not in codes:
                 raise Exception('String is not a DNA sequence')
             
     def gc(self):
-        self = self.upper()
+        self = self.dna.seq
         gc_percent = round(((self.count('G') + self.count('C'))*100)/len(self), 1)
         gc_percent_str = str(gc_percent) + '%'
         return gc_percent_str
     
     def reverse_complement(self):
-        self = self.upper()
+        self = self.dna.seq        
         compl_sequence = self.translate(self.maketrans('ACGTRWSYKMVHDBN', 'TGCAYWSRMKBDHVN'))
         return compl_sequence[::-1]
     
     def transcribe(self):
-        self = self.upper()
+        self = self.dna.seq
         return self.translate(self.maketrans('ACGTRWSYKMVHDBN', 'UGCAYWSRMKBDHVN'))
     
 class Rna(str):
 
     
     def __init__(self, rna_seq):        
-        self.rna_seq = rna_seq
+        self.rna_seq = self.rna.seq
         codes = 'AaCcGgUuRrWwSsYyKkVvHhDdBbNn'
         for i in self:            
             if i not in codes:
                 raise Exception('String is not a RNA sequence')
             
     def gc(self):
-        self = self.upper()
+        self = self.rna.seq
         gc_percent = round((self.count('G') + self.count('C'))*100/len(self), 1)
         gc_percent_str = str(gc_percent) + '%'
         return gc_percent_str
