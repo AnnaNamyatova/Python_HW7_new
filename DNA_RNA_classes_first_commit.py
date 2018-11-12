16886b52c46b9113077fdc6caf3efd71187d8097
AnnaNamyatova <37295536+AnnaNamyatova@users.noreply.github.com>
2018-11-13 00:36:04 +0300

    Update DNA_RNA_classes_first_commit.py

--- DNA_RNA_classes_first_commit.py
+++ DNA_RNA_classes_first_commit.py
@@ -3,16 +3,15 @@ class Dna(str):  #The uploaded DNA should run from 3' to 5' end
     
     def __init__(self, dna_seq):  
         self.dna_seq = dna_seq.upper()
-        codes = 'ACGTRWSYKVHDBN'
+        codes = 'AaCcGgTtRrWwSsYyKkVvHhDdBbNn'
         for i in dna_seq:            
             if i not in codes:
                 raise Exception('String is not a DNA sequence')
             
     def gc(self):
         self = self.dna_seq
-        gc_percent = round(((self.count('G') + self.count('C') + self.count('S')*100)/len(self), 1)
-        gc_percent_str = str(gc_percent) + '%'
-        return gc_percent_str
+        gc_percent = round(((self.count('G') + self.count('C') + self.count('S'))*100)/len(self), 1)        
+        return (str(gc_percent) + '%')
     
     def reverse_complement(self):
         self = self.dna_seq        
@@ -20,14 +19,14 @@ class Dna(str):  #The uploaded DNA should run from 3' to 5' end
         return compl_sequence[::-1]
     
     def transcribe(self):
-        self = self.dna.seq
+        self = self.dna_seq
         return self.translate(self.maketrans('ACGTRWSYKMVHDBN', 'UGCAYWSRMKBDHVN'))
     
 class Rna(str):
 
     
     def __init__(self, rna_seq):        
-        self.rna_seq = self.rna_seq
+        self.rna_seq = rna_seq.upper()
         codes = 'AaCcGgUuRrWwSsYyKkVvHhDdBbNn'
         for i in self:            
             if i not in codes:
@@ -35,9 +34,8 @@ class Rna(str):
             
     def gc(self):
         self = self.rna_seq
-        gc_percent = round((self.count('G') + self.count('C'))*100/len(self), 1)
-        gc_percent_str = str(gc_percent) + '%'
-        return gc_percent_str
+        gc_percent = round((self.count('G') + self.count('C') + self.count('S'))*100/len(self), 1)        
+        return (str(gc_percent) + '%')
     
     def reverse_complement(self):
         self = self.upper()
